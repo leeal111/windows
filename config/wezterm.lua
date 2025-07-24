687b51060bdeaaa7fc64c331d7b284fc44063688
leeal <leeal111@hotmail.com>
2025-07-24 20:23:35 +0800

    修改了 wezterm 快速复制模式的触发快捷键

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,28 +1,30 @@
-local wezterm                       = require 'wezterm'
-local config                        = wezterm.config_builder()
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
 
-config.color_scheme                 = 'Chester'
-config.font                         = wezterm.font 'Cascadia Code NF'
+config.color_scheme = 'Chester'
+config.font = wezterm.font 'Cascadia Code NF'
 
-config.use_fancy_tab_bar            = false
+config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
-config.tab_bar_at_bottom            = true
+config.tab_bar_at_bottom = true
 
-config.default_prog                 = { 'powershell.exe', '-NoLogo' }
-config.default_cwd                  = "C:\\Users\\fumen\\Desktop"
+config.default_prog = {'powershell.exe', '-NoLogo'}
+config.default_cwd = "C:\\Users\\fumen\\Desktop"
 
-config.window_close_confirmation    = 'NeverPrompt'
-config.keys                         = {
-    -- {
-    --     key = 'w',
-    --     mods = 'CTRL|SHIFT',
-    --     action = wezterm.action.CloseCurrentTab { confirm = false },
-    -- },
-    {
-        key = 'w',
-        mods = 'CTRL|SHIFT',
-        action = wezterm.action.CloseCurrentPane { confirm = false },
-    },
-}
+config.window_close_confirmation = 'NeverPrompt'
+config.keys = {{
+    key = 'phys:Space',
+    mods = 'SHIFT|ALT',
+    action = wezterm.action.QuickSelect
+}, {
+    key = 'w',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.CloseCurrentPane {
+        confirm = false
+    }
+}, {
+    key = 'F11',
+    action = wezterm.action.ToggleFullScreen
+}}
 
 return config
